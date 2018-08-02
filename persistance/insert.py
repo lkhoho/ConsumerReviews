4b339f1ec32a9e15f9db2d1d9b7f529f8f1e52bc
Ke Liu <lkhoho@gmail.com>
2018-08-01 22:28:14 -0700

    move data from mongodb to mysql

--- persistance/insert.py
+++ persistance/insert.py
@@ -5,7 +5,7 @@ import pymysql as sql
 
 client = MongoClient()
 db = client.get_database('ConsumerReviews')
-collection_name = 'kbb'
+collection_name = 'bizrate'
 collection = db.get_collection(collection_name)
 docs = list(collection.find())
 client.close()
@@ -49,7 +49,7 @@ insert_kbb_review = "INSERT INTO `KBB_REVIEW`(`CREATE_TIME`, `SOURCE`, `YEAR`, `
 insert_kbb_ratings = "INSERT INTO `KBB_RATINGS`(`CREATE_TIME`, `FK_REVIEW_PID`, `VALUE`, `OVERALL`, `RELIABILITY`, " \
                      "`COMFORT`, `STYLING`, `QUALITY`, `PERFORMANCE`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
-conn = sql.connect(host='10.0.0.32', port=3306, user='root', password='welcome', database='CONSUMER_REVIEWS',
+conn = sql.connect(host='localhost', port=3306, user='root', password='welcome', database='CONSUMER_REVIEWS',
                    charset='utf8mb4')
 cursor = conn.cursor()
 s1, s2, s3 = '', '', ''
@@ -57,9 +57,9 @@ s1, s2, s3 = '', '', ''
 try:
     if collection_name == 'bizrate':
         pass
-        # s1 = insert_bizrate_review
-        # s2 = insert_ratings_site_experience
-        # s3 = insert_ratings_after_purchase
+        s1 = insert_bizrate_review
+        s2 = insert_ratings_site_experience
+        s3 = insert_ratings_after_purchase
     elif collection_name == 'kbb':
         s1 = insert_kbb_review
         s2 = insert_kbb_ratings
@@ -67,39 +67,39 @@ try:
     for doc in docs:
         if collection_name == 'bizrate':
             pass
-            # cursor.execute(s1, (norm_str(doc['scrapedDate']), norm_str(doc['source']), norm_str(doc['reviewStore']),
-            #                     norm_int(doc['wouldShopHereAgain']), norm_int(doc['likelihoodToRecommend']),
-            #                     norm_int(doc['overallSatisfaction']), norm_str(doc['author']), norm_str(doc['date']),
-            #                     norm_str(doc['content']), norm_str(doc['reviewAfterPurchase']['author']),
-            #                     norm_str(doc['reviewAfterPurchase']['date']),
-            #                     norm_str(doc['reviewAfterPurchase']['content'])))
-            # pid = cursor.lastrowid
-            # cursor.execute(s2, (norm_str(doc['scrapedDate']), pid,
-            #                     norm_int(doc['ratingsSiteExperience']['varietyShippingOptions']),
-            #                     norm_int(doc['ratingsSiteExperience']['shippingCharges']),
-            #                     norm_int(doc['ratingsSiteExperience']['easeOfFinding']),
-            #                     norm_int(doc['ratingsSiteExperience']['priceRelativeOtherRetailers']),
-            #                     norm_int(doc['ratingsSiteExperience']['clarityProductInfo']),
-            #                     norm_int(doc['ratingsSiteExperience']['chargesStatedClearly']),
-            #                     norm_int(doc['ratingsSiteExperience']['designSite']),
-            #                     norm_int(doc['ratingsSiteExperience']['productSelection']),
-            #                     norm_int(doc['ratingsSiteExperience']['satisfactionCheckout'])))
-            # cursor.execute(s3, (norm_str(doc['scrapedDate']), pid,
-            #                     norm_int(doc['ratingsAfterPurchase']['productAvailability']),
-            #                     norm_int(doc['ratingsAfterPurchase']['onTimeDelivery']),
-            #                     norm_int(doc['ratingsAfterPurchase']['productMetExpectations']),
-            #                     norm_int(doc['ratingsAfterPurchase']['orderTracking']),
-            #                     norm_int(doc['ratingsAfterPurchase']['returnsProcess']),
-            #                     norm_int(doc['ratingsAfterPurchase']['customerSupport'])))
-            # conn.commit()
-            #
-            # cursor.execute('SELECT COUNT(*) AS c1 FROM `BIZRATE_REVIEW`')
-            # c1 = cursor.fetchone()
-            # cursor.execute('SELECT COUNT(*) AS c2 FROM `BIZRATE_SITE_EXPERIENCE_RATINGS`')
-            # c2 = cursor.fetchone()
-            # cursor.execute('SELECT COUNT(*) AS c3 FROM `BIZRATE_AFTER_PURCHASE_RATINGS`')
-            # c3 = cursor.fetchone()
-            # assert c1[0] == c2[0] == c3[0]
+            cursor.execute(s1, (norm_str(doc['scrapedDate']), norm_str(doc['source']), norm_str(doc['reviewStore']),
+                                norm_int(doc['wouldShopHereAgain']), norm_int(doc['likelihoodToRecommend']),
+                                norm_int(doc['overallSatisfaction']), norm_str(doc['author']), norm_str(doc['date']),
+                                norm_str(doc['content']), norm_str(doc['reviewAfterPurchase']['author']),
+                                norm_str(doc['reviewAfterPurchase']['date']),
+                                norm_str(doc['reviewAfterPurchase']['content'])))
+            pid = cursor.lastrowid
+            cursor.execute(s2, (norm_str(doc['scrapedDate']), pid,
+                                norm_int(doc['ratingsSiteExperience']['varietyShippingOptions']),
+                                norm_int(doc['ratingsSiteExperience']['shippingCharges']),
+                                norm_int(doc['ratingsSiteExperience']['easeOfFinding']),
+                                norm_int(doc['ratingsSiteExperience']['priceRelativeOtherRetailers']),
+                                norm_int(doc['ratingsSiteExperience']['clarityProductInfo']),
+                                norm_int(doc['ratingsSiteExperience']['chargesStatedClearly']),
+                                norm_int(doc['ratingsSiteExperience']['designSite']),
+                                norm_int(doc['ratingsSiteExperience']['productSelection']),
+                                norm_int(doc['ratingsSiteExperience']['satisfactionCheckout'])))
+            cursor.execute(s3, (norm_str(doc['scrapedDate']), pid,
+                                norm_int(doc['ratingsAfterPurchase']['productAvailability']),
+                                norm_int(doc['ratingsAfterPurchase']['onTimeDelivery']),
+                                norm_int(doc['ratingsAfterPurchase']['productMetExpectations']),
+                                norm_int(doc['ratingsAfterPurchase']['orderTracking']),
+                                norm_int(doc['ratingsAfterPurchase']['returnsProcess']),
+                                norm_int(doc['ratingsAfterPurchase']['customerSupport'])))
+            conn.commit()
+
+            cursor.execute('SELECT COUNT(*) AS c1 FROM `BIZRATE_REVIEW`')
+            c1 = cursor.fetchone()
+            cursor.execute('SELECT COUNT(*) AS c2 FROM `BIZRATE_SITE_EXPERIENCE_RATINGS`')
+            c2 = cursor.fetchone()
+            cursor.execute('SELECT COUNT(*) AS c3 FROM `BIZRATE_AFTER_PURCHASE_RATINGS`')
+            c3 = cursor.fetchone()
+            assert c1[0] == c2[0] == c3[0]
         elif collection_name == 'kbb':
             cursor.execute(s1, (norm_str(doc['scrapedDate']), norm_str(doc['source']), norm_str(doc['year']),
                                 norm_str(doc['make']), norm_str(doc['model']), norm_int(doc['mileage']),
