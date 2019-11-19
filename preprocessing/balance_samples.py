1e8e488cb99d30e57f313fe9b490dd44a743a5c4
Ke Liu <lkhoho@gmail.com>
2019-11-18 23:24:14 -0800

    add down-sampling method

--- preprocessing/balance_samples.py
+++ preprocessing/balance_samples.py
@@ -1,53 +1,85 @@
 import os
 import logging
+import enum
 import pandas as pd
-from sklearn import utils
+from typing import Union
+from sklearn.utils import resample
+from airflow.models import Variable
 
 
-def over_sampling_reviews(target_fields: list, store, working_dir, include_index, **context):
+class RebalanceMode(enum.IntEnum):
     """
-    Perform over-sampling to balance positive and negative samples.
-
-    :param target_fields: fields that over-sampling bases on
-    :param store: store name of dataset
-    :param working_dir: directory to save result(s)
-    :param include_index: should include indices of samples in resulting dataset
-    :param context: task instance context
-    :return: paths of resulting files relative to project root
+    Sample rebalance modes.
     """
 
-    exec_date = context['execution_date'].strftime('%Y%m%d')
-    working_dir += os.path.sep + exec_date
-    input_file = context['task_instance'].xcom_pull(task_ids='curate__{}'.format(store))['output_files'][0]
-    logging.info('Input file=' + input_file)
-    logging.info('Target fields=' + str(target_fields))
-    df = pd.read_csv(working_dir + os.sep + input_file)
-    output_files = []
-    for field in target_fields:
-        value_counts = df[field].value_counts()
-        logging.info('Value counts before over-sampling for field {}: pos={}, neg={}'.format(
-            field, value_counts['pos'], value_counts['neg']))
-        majority = 'pos' if value_counts['pos'] > value_counts['neg'] else 'neg'
-        minority = 'neg' if majority == 'pos' else 'pos'
-        logging.info('Majority: {}, minority: {}'.format(majority, minority))
-        df_majority = df[df[field] == majority]
-        df_minority = df[df[field] == minority]
-        df_minority_resampled = utils.resample(df_minority, replace=True, n_samples=df_majority.shape[0],
-                                               random_state=41)
-        df_oversampled = pd.concat([df_majority, df_minority_resampled])
-        fields_to_drop = list(filter(lambda x: x != field, target_fields))
-        df_oversampled.drop(fields_to_drop, axis=1, inplace=True)
-        value_counts = df_oversampled[field].value_counts()
-        assert value_counts['pos'] + value_counts['neg'] == df_oversampled.shape[0]
-        logging.info('Value counts after over-sampling for field: {}: pos={}, neg={}\n'.format(
-            field, value_counts['pos'], value_counts['neg']))
-        dir_path = os.path.sep.join([working_dir, 'balanced'])
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
-        file_name = store + '__' + field + '.csv'
-        df_oversampled.to_csv(os.path.sep.join([dir_path, file_name]), index=include_index)
-        output_files.append(os.path.sep.join(['balanced', file_name]))
-    return {
-        'input_files': [input_file],
-        'output_files': output_files
-    }
+    OVER_SAMPLING = '1'
+    DOWN_SAMPLING = '2'
+
+
+def rebalance_reviews(upstream_task: str, 
+                      label_field: str, 
+                      mode: RebalanceMode, 
+                      positive_label: Union[int, str], 
+                      negative_label: Union[int, str],
+                      random_state=41, 
+                      include_index=False, 
+                      **context):
+    """
+    Perform over-sampling or down-sampling to balance positive and negative reviews.
+    :param upstream_task: Upstream task ID.
+    :param label_field: Column of target variable name.
+    :param mode: Mode of rebalance: over-sampling or down-sampling.
+    :param positive_label: Label for positive samples.
+    :param negative_label: Label for negative samples.
+    :param random_state: Seed of random number. A prime number is preferred. Default is 41.
+    :param include_index: Should include index when saving dataframe.
+    :param context: Jinja template variables in Airflow.
+    :return: Name of data file that has been re-sampled.
+    """
+
+    working_dir = Variable.get('working_dir')
+    os.makedirs(working_dir, exist_ok = True)
+    logging.info('Working dir=' + working_dir)
+    logging.info('Upstream task=' + upstream_task)
+    logging.info('Target variable=' + label_field)
+    logging.info('Rebalance mode={}'.format(mode))
+    logging.info('Positive lable={}. Negative label={}'.format(positive_label, negative_label))
+
+    # if upstream_task is None:
+    #     logging.info('Data pathname=' + data_pathname)
+    #     df = pd.read_csv(data_pathname)
+    # else:
+    filename = context['ti'].xcom_pull(task_ids=upstream_task)
+    logging.info('Input filename=' + filename)
+    df = pd.read_csv(os.sep.join([working_dir, context['ds_nodash'], filename]))
+
+    value_counts = df[label_field].value_counts(sort=True, ascending=False)
+    logging.info('Counts before re-sampling={}'.format(value_counts))
+    if value_counts[positive_label] >= value_counts[negative_label]:
+        major_label = positive_label
+        minor_label = negative_label
+    else:
+        major_label = negative_label
+        minor_label = positive_label
+    df_major = df[df[label_field] == major_label]
+    df_minor = df[df[label_field] == minor_label]
+
+    if mode is RebalanceMode.DOWN_SAMPLING:
+        df_resampled = resample(df_major, replace=True, n_samples=df_minor.shape[0], random_state=random_state)
+        df_result = pd.concat([df_resampled, df_minor])
+    elif mode is RebalanceMode.OVER_SAMPLING:
+        df_resampled = resample(df_minor, replace=True, n_samples=df_major.shape[0], random_state=random_state)
+        df_result = pd.concat([df_resampled, df_major])
+    else:
+        raise ValueError('Unknown rebalance mode: {}'.format(mode))
+    value_counts = df_result[label_field].value_counts()
+    logging.info('Counts after re-sampling={}'.format(value_counts))
+    pos = data_pathname.rfind('/')
+    filename = data_pathname[pos + 1:]
+    pos = filename.rfind('.')
+    filename = filename[:pos] + '__balanced.csv'
+    save_path = os.sep.join([working_dir, context['ds_nodash']])
+    os.makedirs(save_path, exist_ok=True)
+    df_result.to_csv(save_path + os.sep + filename, index=include_index)
+
+    return filename
