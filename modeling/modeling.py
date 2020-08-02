99c0aab4322e1c50f8dc3c1a70c6f8898007ac78
Ke Liu <lkhoho@gmail.com>
2020-08-01 22:18:58 -0700

    add feature to predict and plot

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -1,65 +1,141 @@
 import os
-import re
-import logging
-import simplejson as json
+import argparse
+import json
 import pandas as pd
+import matplotlib.pylab as plt
+from multiprocessing import cpu_count
+from uuid import uuid4
+from typing import List
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import cross_val_score
-import matplotlib.pylab as plt
-from preprocessing.lemmatization import LemmatizationMode
-from preprocessing.feature_selection import FeatureSelectionMode
+from sklearn.svm import SVC
 
 
-def kfold_cv(classifier, classifier_name: str, scorers: list, lem_mode: LemmatizationMode,
-             feature_mode: FeatureSelectionMode, store: str, working_dir: str, **context):
-    exec_date = context['execution_date'].strftime('%Y%m%d')
-    working_dir += os.path.sep + exec_date
-    input_files = context['task_instance'].xcom_pull(
-        task_ids='feature_selection__{}_{}_{}'.format(store, str(lem_mode), str(feature_mode)))['output_files']
-    logging.info('Input files=' + str(input_files))
-    regex = r'.+{}_{}_(?P<label>[A-Z_]+)_(?P<nlp>\w+_\w+)_\w+_(?P<num_features>\d+)\.csv'.format(store, str(lem_mode))
+def parse_cli():
+    parser = argparse.ArgumentParser(description='Build prediction model and plot results.')
+    parser.add_argument('working_dir', help='Working directory')
+    parser.add_argument('data_file', help='TFIDF data file.')
+    parser.add_argument('label', help='Label field name.')
+    parser.add_argument('freq_file', help='File containing list of words ranked by frequency.')
+    parser.add_argument('chi2_file', help='File containing list of words ranked by chi-square.')
+    parser.add_argument('-n', '--num_features',
+                        default='10,25,50,75,100,125,150,175,200,225,250,275,300,350,400,450,500,550,600,650,700,750,'
+                                '800,850,900,950,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2095',
+                        help='# of features as a comma-separated list. Default list '
+                             '"10,25,50,75,100,125,150,175,200,225,250,275,300,350,400,450,500,550,600,650,700,'
+                             '750,800,850,900,950,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2095" '
+                             'will be used if not provided.')
+    parser.add_argument('-s', '--scorers', default='accuracy,balanced_accuracy,roc_auc',
+                        help='Comma-separated scorers. Default scores "accuracy,balanced_accuracy,roc_auc" will be '
+                             'used if not provided.')
+    parser.add_argument('-x', '--x_lim', default='0,2200',
+                        help='Comma-separated range of X axis in plot. Default [0.0, 2200.0] will be used '
+                             'if not provided.')
+    parser.add_argument('-y', '--y_lim', default='0,1',
+                        help='Comma-separated range of Y axis in plot. Default [0.0, 1.0] will be used '
+                             'if not provided.')
+    return parser.parse_args()
+
+
+def kfold_cv(classifier,
+             tfidf: pd.DataFrame,
+             all_features: List[str],
+             num_features: List[int],
+             scorers: List[str]):
     le = LabelEncoder()
+    y = le.fit_transform(tfidf[args.label])
+    X = tfidf.drop(columns=[args.label])
+
+    results = {
+        scorer: {
+            str(num): {
+                'raw': 0,
+                'mean': 0
+            } for num in num_features
+        } for scorer in scorers
+    }
+
+    for num in num_features:
+        features = all_features[:num]
+        for scorer in scorers:
+            print('Running k-fold CV: num_features={}, scorer={}'.format(num, scorer))
+            scores = cross_val_score(classifier, X[features], y, cv=10, scoring=scorer, n_jobs=cpu_count())
+            results[scorer][str(num)]['raw'] = scores.tolist()
+            results[scorer][str(num)]['mean'] = scores.mean()
+
+    return results
+
+
+if __name__ == '__main__':
+    args = parse_cli()
+
+    print('Working directory: {}'.format(args.working_dir))
+    print('TFIDF file: {}'.format(args.data_file))
+    print('Label field: {}'.format(args.label))
+
+    freq_features = []
+    with open(os.sep.join([args.working_dir, args.freq_file])) as fp:
+        for line in fp:
+            freq_features.append(line.strip())
+    print('{} features in frequency file: {}'.format(len(freq_features), args.freq_file))
+
+    chi2_features = []
+    with open(os.sep.join([args.working_dir, args.chi2_file])) as fp:
+        for line in fp:
+            chi2_features.append(line.strip())
+    print('{} features in chi-square file: {}'.format(len(chi2_features), args.chi2_file))
+
+    num_features = [int(x) for x in args.num_features.split(',')]
+    print('# of features: {}'.format(num_features))
+
+    scorers = args.scorers.split(',')
+    print('scorers: {}'.format(scorers))
+
+    x_lim = tuple(float(x) for x in args.x_lim.split(','))
+    y_lim = tuple(float(y) for y in args.y_lim.split(','))
+    print('X axis range: {}'.format(x_lim))
+    print('Y axis range: {}'.format(y_lim))
+
+    df = pd.read_csv(os.sep.join([args.working_dir, args.data_file]))
+    print('Dataframe shape={}'.format(df.shape))
 
     results = {}
-    for file in input_files:
-        label = re.match(regex, file).group('label')
-        nlp = re.match(regex, file).group('nlp')
-        num_features = re.match(regex, file).group('num_features')
-        logging.info('label={}, nlp={}, num_features={}'.format(label, nlp, str(num_features)))
 
-        base_key = '__'.join([store, str(lem_mode), label, nlp, str(feature_mode)])
-        df = pd.read_csv(working_dir + os.path.sep + file)
-        y = le.fit_transform(df[label])
-        X = df.loc[:, df.columns != label]
-        for scorer in scorers:
-            scores = cross_val_score(classifier, X, y, cv=10, scoring=scorer)
-            results.setdefault(base_key, {}).setdefault(scorer, {}).setdefault(str(num_features), {})['raw'] = scores.tolist()
-            results[base_key][scorer][str(num_features)]['mean'] = scores.mean()
+    print('Running K-fold CV for frequency ranking ...')
+    freq_results = kfold_cv(SVC(kernel='linear', C=1),
+                            tfidf=df,
+                            all_features=freq_features,
+                            num_features=num_features,
+                            scorers=scorers)
+    results['freq'] = freq_results
 
-    filename = '{store}_{lem_mode}_{feature_mode}_{classifier_name}.json'\
-        .format(store=store, lem_mode=str(lem_mode), feature_mode=str(feature_mode), classifier_name=classifier_name)
-    with open(os.path.sep.join([working_dir, filename]), 'w') as fp:
+    print('Running K-fold CV for chi2 ranking ...')
+    chi2_results = kfold_cv(SVC(kernel='linear', C=1),
+                            tfidf=df,
+                            all_features=chi2_features,
+                            num_features=num_features,
+                            scorers=scorers)
+    results['chi2'] = chi2_results
+    score_filename = '{}.json'.format(uuid4())
+    with open(os.sep.join([args.working_dir, score_filename]), 'w') as fp:
         json.dump(results, fp)
-    plot_graph(os.path.sep.join([working_dir, filename]), scorers, working_dir)
+    print('Save score results to file {}'.format(score_filename))
 
+    # start plotting
+    dir_path = os.sep.join([args.working_dir, 'figures'])
+    os.makedirs(dir_path, exist_ok=True)
 
-def plot_graph(data_file: str, scorers: list, working_dir: str):
-    with open(data_file) as fp:
-        data = json.load(fp)
-
-    dir_path = os.path.sep.join([working_dir, 'figures'])
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    for graph_name, graph_data in data.items():
+    for ranking in {'freq', 'chi2'}:
         for scorer in scorers:
             fig = plt.figure()
-            scorer_data = graph_data[scorer]
-            num_features = sorted([int(x) for x in scorer_data.keys()])
-            values = [scorer_data[str(x)]['mean'] for x in num_features]
-            plt.plot(num_features, values, 'bo')
-            plt.ylim(0.0, 1.0)
+            scorer_data = results[ranking][scorer]
+            n_feats = sorted([int(x) for x in scorer_data.keys()])
+            values = [scorer_data[str(x)]['mean'] for x in n_feats]
+            plt.plot(n_feats, values, 'bo-')
+            plt.xlim(x_lim)
+            plt.ylim(y_lim)
             plt.xlabel('# of features')
-            plt.ylabel('mean of score')
-            plt.title(graph_name)
-            fig.savefig(os.path.sep.join([dir_path, graph_name + '_' + scorer + '.png']))
-    logging.info('Done plotting!')
+            plt.ylabel('performance')
+            title = 'Mean scores for {} {}'.format(ranking, scorer)
+            fig.savefig(os.sep.join([dir_path, title + '.png']))
+    print('Done!')
